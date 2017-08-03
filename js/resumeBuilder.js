bd7fc0c4cbd78550a2026d06005c1a837c7d48ee
vinicius_lara <vlara@avenuecode.com>
2017-08-03 18:14:57 -0300

    First steps with JS

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//$("#main").append("Vinicius Lara");
+
+//var email = "vinicius.lara@gmail.com";
+//var newEmail = email.replace("lara", "vilacalara");
+//console.log(email);
+//console.log(newEmail);
+
+//var awesomeThoughts = "I am the Vinicius and I am AWESOME";
+//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+//console.log(funThoughts);
+
+//$("#main").append(funThoughts);
+
+var name = "Vinicius Antonio Vilaça Lara"
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").append(formattedName);
