903913d35a2134524a11ff3440abb9847bd7d94e
vinicius_lara <vlara@avenuecode.com>
2017-08-11 19:06:11 -0300

    Added array methods

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,29 @@
-//$("#main").append("Vinicius Lara");
-
-//var email = "vinicius.lara@gmail.com";
-//var newEmail = email.replace("lara", "vilacalara");
-//console.log(email);
-//console.log(newEmail);
-
-//var awesomeThoughts = "I am the Vinicius and I am AWESOME";
-//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-//console.log(funThoughts);
-
-//$("#main").append(funThoughts);
 
 var name = "Vinicius Antonio Vilaça Lara"
 var formattedName = HTMLheaderName.replace("%data%", name);
-
 
 var role = "QA Engineer"
 var formattedRole = HTMLheaderRole.replace("%data%", role)
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var audacity = "audacity"
+var converted = audacity.slice(1,10)
+converted = converted.charAt(0).toUpperCase + converted.slice(1)
+console.log(converted)
+
+
+var name = "AlbeRt EINstEiN"
+var arrayName = name.split(" ")
+var firstNameSecondPart = arrayName[0].slice(1)
+firstNameSecondPart = firstNameSecondPart.toLowerCase()
+console.log(firstNameSecondPart)
+var firstName = arrayName[0].charAt(0).toUpperCase() + firstNameSecondPart
+
+var secondName = arrayName[1].toUpperCase();
+
+var finalName = firstName +  " " + secondName
+
+console.log(finalName)
+
+
